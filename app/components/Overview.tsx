feee396deb37db985fec183b44ece605f67dabd8
hakeem-jc <hakeemclarke14@gmail.com>
2025-03-08 17:58:19 -0500

    feat: Style currency converter

--- app/components/Overview.tsx
+++ app/components/Overview.tsx
@@ -47,36 +47,42 @@ const Overview = () => {
         </div>
       </div>
 
-      <div className="max-w-sm min-h-48 p-6 rounded-lg shadowm-sm bg-[#212121] flex flex-col justify-between">
-        <div className="flex gap-3 w-full mb-4">
+      <div className="max-w-sm min-h-48 p-6 rounded-lg shadowm-sm bg-[#212121] flex flex-col justify-between items-center gap-8">
+        <div className="flex gap-3 w-full">
           <p className="block">Your Wallet</p>
-          <p className="block text-gray-400">Buy</p>
-          <p className="block text-gray-400">Sell</p>
+          <p className="block text-gray-400">Fiat</p>
+          <p className="block text-gray-400">Crypto</p>
         </div>
 
         <div className="flex w-full justify-between">
           <div>
-            <p className="text-sm text-gray-400">You Pay</p>
-            <h2 className="text-lg">3</h2>
+            <p className="text-sm text-gray-400 mb-2">Quote Currency</p>
+            <input type="number" className="bg-[#181818] text-white" />
           </div>
 
           <div className="bg-[#181818] p-2 rounded-lg">
-            <p className="text-sm text-gray-400">ETH</p>
+            <select className="bg-[#181818] text-white">
+              <option>JMD</option>
+              <option>EUR</option>
+            </select>
           </div>
         </div>
-        <button>Temp</button>
+
         <div className="flex w-full justify-between">
           <div>
-            <p className="text-sm text-gray-400">You Pay</p>
-            <h2 className="text-lg">250</h2>
+            <p className="text-sm text-gray-400 mb-2">Base Currency</p>
+            <input type="number" className="bg-[#181818] text-white" />
           </div>
 
           <div className="bg-[#181818] p-2 rounded-lg">
-            <p className="text-sm text-gray-400">USDT</p>
+            <select className="bg-[#181818] text-white">
+              <option>EUR</option>
+              <option>JMD</option>
+            </select>
           </div>
         </div>
 
-        <p className="text-gray-400 text-sm">1USDT = 0.00023ETH</p>
+        <p className="text-gray-400 text-sm">1 JMD = 0.005 EUR</p>
       </div>
     </section>
   );
